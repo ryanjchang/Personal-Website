38974f28dbb016d759b85ce6d7f1574a7fccee8d
ryanjchang <ryanjchang@gmail.com>
2026-07-03 13:51:07 -0400

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -52,6 +52,13 @@ export const Navbar = () => {
             whileHover={{scale: 1.1}}
             whileTap={{scale:0.95}}
             >
+                <a href="#class-projects">Class Projects</a>
+            </motion.li>
+            <motion.li 
+            variants={fadeInUp}
+            whileHover={{scale: 1.1}}
+            whileTap={{scale:0.95}}
+            >
                 <a href="#contact"> Contact</a>
             </motion.li>
 
